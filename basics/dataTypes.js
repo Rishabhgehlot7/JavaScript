918ca42413ef00dc5738863204ba66226fd2b522
Rishabh gehlot <92848940+Rishabhgehlot7@users.noreply.github.com>
2023-10-13 11:17:35 +0000

    2 day

---
+++ basics/dataTypes.js
@@ -0,0 +1,14 @@
+// datatypes in javaScript
+
+// Number
+// BigInt
+// String
+// undefined
+// null
+// Boolean
+// Symbol
+
+// Object
+
+console.table([typeof 123, typeof BigInt(123), typeof "hello", typeof undefined, typeof null, typeof true, typeof Symbol("RAM")])
+console.log(typeof { ram: "ram", "rishabh": "radhe" })
---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const employeeId = 125;
+let employeeEmail = "rishabh@gmail.com";
+var employeeNumber = 789456;
+employeeCity = "jodhpur";
+
+console.table([employeeId, employeeEmail, employeeNumber, employeeCity]);
